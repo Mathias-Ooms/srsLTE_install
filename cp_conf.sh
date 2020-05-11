633a0fd244490db47b733b816788f12565ceaa0c
DawidMiroyan <dawid_miroyan@hotmail.com>
2020-05-11 09:23:46 -0600

    more fixes to configuration and cp

--- cp_conf.sh
+++ cp_conf.sh
@@ -8,4 +8,4 @@ sudo cp ./conf/* $HOME/.config/srslte/
 
 # Use ueXOR as default user equipment
 sudo cp /etc/srslte/ueXOR.conf /etc/srslte/ue.conf
-sudo cp /etc/srslte/ueXOR.conf $HOME/.config/srslte
+sudo cp /etc/srslte/ueXOR.conf $HOME/.config/srslte/ue.conf

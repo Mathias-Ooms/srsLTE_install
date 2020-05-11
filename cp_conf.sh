754c0460c9870cc24403ddd7dabd80fbcae2f959
s0165805 <s0165805@ue.anlgrp2v5.ilabt-imec-be.wall1.ilabt.iminds.be>
2020-05-11 08:25:29 -0600

    Fixed copy script, copies to /etc/srslte and .config/srslte

---
+++ cp_conf.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo mkdir /etc/srslte
+sudo mkdir $HOME/.config/srslte
+
+sudo cp ./conf/* /etc/srslte/
+sudo cp ./conf/* $HOME/.config/srslte/
+
+# Use ueXOR as default user equipment
+sudo cp /etc/srslte/ueXOR.conf /etc/srslte/ue.conf
+sudo cp /etc/srslte/ueXOR.conf $HOME/.config/srslte
--- mv_conf.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo mv --parents conf/* /etc/srslte
-
-# Use ueXOR as default user equipment
-sudo cp /etc/srslte/ueXOR.conf /etc/srslte/ue.conf

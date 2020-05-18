10e0d7da58aa50b8efbfbcc2133569f02e5ac178
DawidMiroyan <dawid_miroyan@hotmail.com>
2020-05-18 12:21:24 +0200

    Aaaa

--- startup.sh
+++ startup.sh
@@ -5,65 +5,44 @@ if [[ $(/usr/bin/id -u) -ne 0 ]]; then
     exit
 fi
 
-if [ $# -eq 0 ]
-  then
-    echo "Specify whether the node is 'ue', 'enb', 'epc' or 'media'"
-    echo "Ex. sudo ./startup ue"
-    exit 1
-fi
-
 # Get access to the internet
 wget -O - -q https://www.wall2.ilabt.iminds.be/enable-nat.sh | sudo bash
 
-#sudo route del default gw 10.2.15.254 ; sudo route add default gw 10.2.15.253
-#sudo route add -net 10.11.0.0 netmask 255.255.0.0 gw 10.2.15.254
-#sudo route add -net 10.2.32.0 netmask 255.255.240.0 gw 10.2.15.254
+sudo route del default gw 10.2.15.254 ; sudo route add default gw 10.2.15.253
+sudo route add -net 10.11.0.0 netmask 255.255.0.0 gw 10.2.15.254
+sudo route add -net 10.2.32.0 netmask 255.255.240.0 gw 10.2.15.254
 
-sudo apt update #&& apt upgrade
+sudo apt-get -y update
+sudo apt-get -y install cmake libfftw3-dev libmbedtls-dev libboost-program-options-dev libconfig++-dev libsctp-dev
+sudo apt-get -y install libzmq3-dev
+sudo apt-get -y install libboost-system-dev libboost-test-dev libboost-thread-dev libqwt-dev libqt4-dev
 
-# Install required libraries
-sudo apt-get install cmake libfftw3-dev libmbedtls-dev libboost-program-options-dev libconfig++-dev libsctp-dev -y
+mkdir build_srslte
+cd build_srslte
+git clone https://github.com/srsLTE/srsGUI.git
+cd srsGUI
+mkdir build
+cd build
+cmake ../
+make
+sudo make install
 
-cd $HOME
-
-# Install zeromq on the enb and ue  nodes
-if [ $1 == 'ue' ] || [ $1 == 'enb' ]
-then
-	# libzmq
-	git clone https://github.com/zeromq/libzmq.git
-	cd libzmq
-	./autogen.sh
-	./configure
-	make
-	sudo make install
-	sudo ldconfig
-	cd ../
-
-	# czmq
-	git clone https://github.com/zeromq/czmq.git
-	cd czmq
-	./autogen.sh
-	./configure
-	make
-	sudo make install
-	sudo ldconfig
-	cd ../
-fi
-
-# Install srsLTE on all LTE nodes
-if [ $1  != "media" ]
-then
-	# srsLTE
-	git clone https://github.com/mu1tiplex/srsLTE-anl.git
-	cd srsLTE-anl
-	mkdir build
-	cd build
-	cmake ../
-	make
-	sudo make install
-	sudo ldconfig
-	sudo srslte_install_configs.sh user
-	cd ../../
-fi
+cd
+cd build_srslte
+git clone https://github.com/mu1tiplex/srsLTE-anl.git
+cd srsLTE-anl
+mkdir build
+cd build
+cmake ../
+make
+sudo make install
+sudo ldconfig
+sudo srslte_install_configs.sh user
 
 # TODO Move srs config files
+cd
+cd build_srslte
+git clone https://github.com/Mathias-Ooms/srsLTE_install.git
+cd srsLTE_install
+sudo cp_conf.sh
+sudo 
